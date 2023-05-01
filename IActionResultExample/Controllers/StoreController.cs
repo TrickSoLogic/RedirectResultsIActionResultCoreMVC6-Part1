b0f1151ea809b9eadcd7a01e96f5894cae9e7c48
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-05-01 20:03:33 +0530

    Add project files.

---
+++ IActionResultExample/Controllers/StoreController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedirectResultsIActionResultCoreMVC6.Controllers
+{
+  public class StoreController : Controller
+  {
+    [Route("store/books")]
+    public IActionResult Books()
+    {
+      return Content("<h1>Book Store</h1>", "text/html");
+    }
+  }
+}
